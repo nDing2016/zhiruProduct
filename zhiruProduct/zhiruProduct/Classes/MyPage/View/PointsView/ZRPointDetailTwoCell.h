f7964b80f8d5677270ce3e38f55c7f672d62eb35
pj <iosdevelopvip@163.com>
2016-12-07 17:48:03 +0800

    pj

--- zhiruProduct/zhiruProduct/Classes/MyPage/View/PointsView/ZRPointDetailTwoCell.h
+++ zhiruProduct/zhiruProduct/Classes/MyPage/View/PointsView/ZRPointDetailTwoCell.h
@@ -2,14 +2,19 @@
 //  ZRPointDetailTwoCell.h
 //  zhiruProduct
 //
-//  Created by pj on 16/12/5.
+//  Created by pj on 16/12/7.
 //  Copyright © 2016年 Zhiru. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import "ZRLunchView.h"
 #import "ZRPointOrderDetailModel.h"
+#import "ZRPointView.h"
 @interface ZRPointDetailTwoCell : UITableViewCell
 
+@property (nonatomic, strong)UILabel * titlaLabel;
+@property (nonatomic, strong)ZRPointView * orderName;
+@property (nonatomic, strong)ZRPointView * orderPhone;
+@property (nonatomic, strong)ZRLunchView * orderAddress;
 @property (nonatomic, strong)ZRPointOrderDetailModel * model;
-
 @end
